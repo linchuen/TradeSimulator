7bc9f0533eff75bdf0fe8c3d5021fbf46d949ac0
Aiden <x29587629@gmail.com>
2023-07-03 21:30:14 +0800

    git init

---
+++ TradeSystem/src/main/java/com/cooba/TradeSimulator/TradeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cooba.TradeSimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeSystemApplication.class, args);
+	}
+
+}
