6a219574324fa045986743f42d32268f48a995f8
Aiden <aiden.lin@ambernet.com.tw>
2023-08-31 18:07:17 +0800

    add RedisUtil

--- CurrencySystem/src/main/java/com/cooba/TradeSimulator/DataLayer/CurrencyDB.java
+++ CurrencySystem/src/main/java/com/cooba/TradeSimulator/DataLayer/CurrencyDB.java
@@ -4,13 +4,15 @@ import com.cooba.TradeSimulator.Entity.Currency;
 import com.cooba.TradeSimulator.Enum.DefaultCurrency;
 import com.cooba.TradeSimulator.Mapper.CurrencyDynamicSqlSupport;
 import com.cooba.TradeSimulator.Mapper.CurrencyMapper;
-import lombok.AllArgsConstructor;
+import com.cooba.TradeSimulator.Util.RedisUtil;
 import org.mybatis.dynamic.sql.SqlBuilder;
 import org.mybatis.dynamic.sql.render.RenderingStrategies;
 import org.mybatis.dynamic.sql.select.render.SelectStatementProvider;
 import org.mybatis.dynamic.sql.update.render.UpdateStatementProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
@@ -20,9 +22,13 @@ import static com.cooba.TradeSimulator.Mapper.CurrencyDynamicSqlSupport.currency
 import static org.mybatis.dynamic.sql.SqlBuilder.isEqualTo;
 
 @Service
-@AllArgsConstructor
 public class CurrencyDB {
-    private final CurrencyMapper currencyMapper;
+    @Autowired
+    private CurrencyMapper currencyMapper;
+    @Autowired
+    private RedisUtil redisUtil;
+
+    private final String redisKey = "currency";
 
     public boolean insert(Currency currency) {
         Map<String, DefaultCurrency> defaultCurrencyMap = DefaultCurrency.getCurrencyNameMap();
@@ -30,6 +36,7 @@ public class CurrencyDB {
         currency.setId(defaultCurrency == null ? null : defaultCurrency.getId());
         currency.setCreatedTime(LocalDateTime.now());
         currency.setUpdatedTime(LocalDateTime.now());
+        redisUtil.put(redisKey, currency.getName(), currency.getRate().toPlainString());
         return currencyMapper.insert(currency) == 1;
     }
 
@@ -39,6 +46,7 @@ public class CurrencyDB {
                 .set(CurrencyDynamicSqlSupport.updatedTime).equalTo(LocalDateTime.now())
                 .where(CurrencyDynamicSqlSupport.name, isEqualTo(currency.getName()))
                 .build().render(RenderingStrategies.MYBATIS3);
+        redisUtil.put(redisKey, currency.getName(), currency.getRate().toPlainString());
         return currencyMapper.update(query) == 1;
     }
 
@@ -52,5 +60,17 @@ public class CurrencyDB {
 
     public Optional<Currency> selectById(Integer currencyId) {
         return currencyMapper.selectByPrimaryKey(currencyId);
+    }
+
+    public BigDecimal getRateByName(String currencyName) {
+        String rate = redisUtil.get(redisKey, currencyName);
+        if (rate != null) return new BigDecimal(rate);
+
+        SelectStatementProvider query = SqlBuilder.select(currency.rate)
+                .from(currency)
+                .where(CurrencyDynamicSqlSupport.name, isEqualTo(currencyName))
+                .build().render(RenderingStrategies.MYBATIS3);
+        Optional<Currency> currencyOptional = currencyMapper.selectOne(query);
+        return currencyOptional.map(Currency::getRate).orElse(null);
     }
 }
---
+++ common/src/main/java/com/cooba/TradeSimulator/Util/RedisUtil.java
@@ -0,0 +1,22 @@
+package com.cooba.TradeSimulator.Util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedisUtil {
+    @Autowired(required = false)
+    private RedisTemplate<String, String> redisTemplate;
+    @Value("${spring.redisson.enable}")
+    private boolean redissonEnable;
+
+    public void put(String key, String hashKey, String value) {
+        redisTemplate.opsForHash().put(key, hashKey, value);
+    }
+
+    public String get(String key, String hashKey) {
+        return (String) redisTemplate.opsForHash().get(key, hashKey);
+    }
+}
